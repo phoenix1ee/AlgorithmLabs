9c91db467f790b28824258b285d0368236bd7f7f
phoenix1ee <110279025+phoenix1ee@users.noreply.github.com>
2025-09-08 21:28:55 -0700

    add test scripts

--- ProgrammingAssignment1/manhattan/dummy.py
+++ ProgrammingAssignment1/manhattan/dummy.py
@@ -1,10 +1,25 @@
-def coordinates(in_str:str):
-    delimiter=in_str.find(",")
-    if in_str[:delimiter].isnumeric and in_str[delimiter+1:].isnumeric:
-        return int(in_str[:delimiter]), int(in_str[delimiter+1:])
-    else:
-        return None
+def readPoints(input_f):
+    """
+    read input file containing lines of coordinates points 
+    example line:1,3
+    convert and output a list of input data
+    :param input_f :a file object which contain some coordinates points
+    :return: the list of input data
+    """
+    points = []
+    error = []
+    line = 0
+    for l in input_f:
+        line += 1
+        in_c = l.strip().replace(" ", "")
+        if in_c and in_c.isnumeric():
+            pass
+        else:
+            if len(in_c) == 0:
+                error.append((line, "empty line"))
+            else:
+                error.append((line, "invalid line"))
+    return points, error
     
 test="1,34"
-x,y=coordinates(test)
-print(x,y)
+
