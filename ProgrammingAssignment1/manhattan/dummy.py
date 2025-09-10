4458a54a78fdf70d5a8965170bc68cdceebe1850
phoenix1ee <110279025+phoenix1ee@users.noreply.github.com>
2025-09-09 22:44:07 -0700

    add File IO
    add File IO
    updated main

--- ProgrammingAssignment1/manhattan/dummy.py
+++ ProgrammingAssignment1/manhattan/dummy.py
@@ -1,25 +1,22 @@
-def readPoints(input_f):
-    """
-    read input file containing lines of coordinates points 
-    example line:1,3
-    convert and output a list of input data
-    :param input_f :a file object which contain some coordinates points
-    :return: the list of input data
-    """
-    points = []
-    error = []
-    line = 0
-    for l in input_f:
-        line += 1
-        in_c = l.strip().replace(" ", "")
-        if in_c and in_c.isnumeric():
-            pass
-        else:
-            if len(in_c) == 0:
-                error.append((line, "empty line"))
-            else:
-                error.append((line, "invalid line"))
-    return points, error
-    
-test="1,34"
+from pathlib import Path
 
+def writeResults(output_f, result):
+    """
+    obtain an output file and write the result list to the output file
+    example line:1,3
+    :param output_f :a file object for written to
+    :param result : the list containing the output data
+    :return: None
+    """
+    with (output_f.open('w') as file):
+        for x in result:
+            file.write(str(x))
+            file.write("\n")
+
+    
+# Testing
+if __name__ == "__main__":
+    test1=[[[1, 2], [1, 3]], [[1, 2], [3, 1]], [[1, 3], [3, 1]], [[1, 3], [4, 4]]]
+    outpath = Path('ProgrammingAssignment1\input\whiteBox1_output.txt')
+    writeResults(outpath,test1)
+    
--- ProgrammingAssignment1/manhattan/manhattan_main.py
+++ ProgrammingAssignment1/manhattan/manhattan_main.py
@@ -6,7 +6,7 @@ setrecursionlimit(30000)
 from pathlib import Path
 import argparse
 from time import time_ns
-
+import manhattan
 
 def coordinates(in_str:str):
     delimiter=in_str.find(",")
@@ -16,7 +16,7 @@ def coordinates(in_str:str):
         return None
 
 
-def readPoints(input_f):
+def readPoints(in_path):
     """
     read input file containing lines of coordinates points 
     example line:1,3
@@ -25,19 +25,13 @@ def readPoints(input_f):
     :return: the list of input data
     """
     points = []
-    error = []
-    line = 0
-    for l in input_f:
-        line += 1
-        in_c = l.strip().replace(" ", "")
-        if in_c and in_c.isnumeric():
-            pass
-        else:
-            if len(in_c) == 0:
-                error.append((line, "empty line"))
-            else:
-                error.append((line, "invalid line"))
-    return points, error
+    print(in_path)
+    with open(in_path, mode='r', encoding='utf-8', newline='') as file:
+        reader = csv.reader(file, delimiter=',')  # Change delimiter if needed
+        for row in reader:
+            if row:
+                points.append([int(row[0]),int(row[1])])
+    return points
 
 def writeResults(output_f, result):
     """
@@ -47,6 +41,19 @@ def writeResults(output_f, result):
     :param result : the list containing the output data
     :return: None
     """
+    with (output_f.open('w') as file):
+        for x in result:
+            file.write(str(x))
+            file.write("\n")
+
+def writeAnalysis(output_f, result):
+    """
+    write analysis to output file
+    example line:inputfilename, data size, m size, processing time(ns)
+    :param output_f :a file object for written to
+    :param result : the list containing an entry of analysis data
+    :return: None
+    """
     pass
 
 # use the Argument parser to define compulsory and optional arguments
@@ -54,54 +61,74 @@ this_parser = argparse.ArgumentParser(description ='calculate manhattan distance
 this_parser.add_argument('-l', action='store_true', help="Optional, to support batch input by inputing a file containing list of local files")
 this_parser.add_argument('n_file', type=str, help="Input File containing the coordinates")
 this_parser.add_argument('m', type=int, help="No. of closet pairs needed")
+this_parser.add_argument('-o', action='store_true', help="Optional, print the manhattan result list to file")
+this_parser.add_argument('-r', action='store_true', help="Optional, repeat manhattan process for accurate analysis")
 this_parser.add_argument('-a', action='store_true', help="Optional, print to include the analysis details in output")
+
 args = this_parser.parse_args()
 
 # Set the input and output file path
 in_path = Path(args.n_file)
 
+if not args.m.isnumeric():
+    raise Exception(f'argument for m, {args.m}, is not valid, expect integer')
+
 if in_path.is_file():
+    #read the points from file
+
     #proceed to start calculation and procesisng if input file exists, and return analysis data if applicable
-    with (in_path.open('r') as in_file):
-        if not args.l:
+    
+    #write the results to file if necessary
+    #start if input file exists
+    if not args.l:
+        #read the points from file
+        coordinate = readPoints(in_path)
+        #start calculation and time the process
+        start_time = time_ns()
+        result = manhattan(coordinate,int(args.m))
+        end_time = time_ns()
+        #set output file
+        if args.o:
             filename = str(in_path.name)
             out_path = str(in_path.absolute())
             out_pathdir = out_path[:len(out_path) - len(filename)]
             out_pathreal = Path(out_pathdir + "output_"+ "_" + filename)
-            output_file = out_pathreal.open('w')
-
-            start_time = time_ns()
-            num_list, compare, exchange = sortthisway(in_file,args.t,in_path.absolute())
-            printout(num_list, compare, exchange, output_file, in_path.absolute())
-            end_time = time_ns()
-            if args.a:
-                print("processing statistics:")
-                print("file, sort type, no. of comparison, no. of exchange, processing time(ns)")
-                print(args.n_file, args.t, compare, exchange, "%.2f" % (end_time - start_time), sep=",")
-        else:
-            filelist=[]
+            writeResults(out_pathreal,result)
+        if args.a:
+            print("processing statistics:")
+            print("file, data size, m size, processing time(ns)")
+            print(args.n_file, len(coordinate), args.m, "%.2f" % (end_time - start_time), sep=",")
+    else:
+        filelist=[]
+        with (in_path.open('r') as in_file):
             for line in in_file:
                 line_s = line.strip().replace(" ", "")
                 if line_s:
                     filelist.append(line_s)
             if args.a:
                 print("processing statistics:")
-                print("file, sort type, no. of comparison, no. of exchange, processing time(ns)")
+                print("file, data size, m size, processing time(ns)")
             for f_name in filelist:
                 f_path = Path(f_name)
                 if f_path.is_file():
+                    #read the points from file
+                    coordinate = readPoints(f_path)
+                    #start calculation and time the process
+                    start_time = time_ns()
+                    result = manhattan(coordinate,int(args.m))
+                    end_time = time_ns()
+                    #set output file
                     with (f_path.open('r') as in_file2):
                         start_time = time_ns()
                         num_list, compare, exchange  = sortthisway(in_file2,args.t,f_path.absolute())
                         filename = str(f_path.name)
-                        out_path = str(f_path.absolute())
-                        out_pathdir = out_path[:len(out_path) - len(filename)]
-                        out_pathreal = Path(out_pathdir + "output_" + args.t + "_"  + filename)
-                        output_file = out_pathreal.open('w')
-                        printout(num_list,compare, exchange, output_file,f_path.absolute())
-                        end_time = time_ns()
+                        if args.o:
+                            out_path = str(f_path.absolute())
+                            out_pathdir = out_path[:len(out_path) - len(filename)]
+                            out_pathreal = Path(out_pathdir + "output_" + args.t + "_"  + filename)
+                            writeResults(out_pathreal,result)
                         if args.a:
-                            print(f_name,args.t,compare,exchange,"%.2f" % (end_time - start_time),sep=",")
+                            print(args.n_file, len(coordinate), args.m, "%.2f" % (end_time - start_time), sep=",")
                 else:
                     print(f'{f_path.absolute()} in file list supplied do not exist')
 else:
