aca3aa5a2e30e1e2bfdeb8e91e378f3c8c4227aa
phoenix1ee <110279025+phoenix1ee@users.noreply.github.com>
2025-10-17 13:33:35 -0700

    draft enhancement-step 2 reduction

--- Markov/markovChainEnhanced.py
+++ Markov/markovChainEnhanced.py
@@ -96,6 +96,11 @@ def partition(A:list, i:int, ii:int, j:int, jj:int):
     """
     return([x[slice(j,jj)] for x in A[slice(i,ii)]])
 
+def inverse2x2(A:list):
+    deter = A[0][0]*A[1][1]-A[0][1]*A[1][0]
+    output = [[A[1][1],-A[0][1]],[-A[1][0],A[0][0]]]
+    return scalarMul(output,deter)
+
 # use the Argument parser to define compulsory and optional arguments
 this_parser = argparse.ArgumentParser(description ='Find Markov Chain state probability: csv file represeting input state matrix')
 this_parser.add_argument('n_file', type=str, help="Input File containing the matrix")
@@ -120,22 +125,31 @@ if in_path.is_file():
     W = dict()
     R = dict()
     Q = dict()
-    while len(temp)>1:
-        W.update({len(temp):partition(temp,0,-1,-1,None)})
-        R.update({len(temp):partition(temp,-1,None,0,-1)})
-        Q.update({len(temp):temp[-1][-1]})
-        T = partition(temp,0,-1,0,-1)
-        IQ = 1/(1-Q[len(temp)])
-        temp = madd(mmul(scalarMul(W[len(temp)],IQ),R[len(temp)]),T)
+    while len(temp)>2:
+        r = len(temp)
+        W.update({r:partition(temp,0,-2,-2,None)})
+        R.update({r:partition(temp,-2,None,0,-2)})
+        Q.update({r:partition(temp,-2,None,-2,None)})
+        T = partition(temp,0,-2,0,-2)
+        IQ = inverse2x2(msub(Imatrix(2),Q[r]))
+        temp = madd(mmul(mmul(W[r],IQ),R[r]),T)
         print(f"P{len(temp)}:")
         output.append(f"P{len(temp)}:")
         mprint(temp)
         for x in temp:
             output.append(x)
+    
     #enlargement
     print("matrix enlargement: ")
     output.append("matrix enlargement: ")
     k = [1]
+    #ending matrix is 2x2:
+    if len(temp)==2:
+        k2 = 1/(1-temp[1][1])*temp[0][1]
+        k.append(k2)
+        print(f"k2={k2}")
+        output.append(f"k2={k2}")
+    """
     for i in range(1,size):
         n = i+1
         temp = 1/(1-Q[n])*vectMul(k,GetCol(W[n],-1))
@@ -165,100 +179,6 @@ if in_path.is_file():
             for x in output:
                 file.write(str(x))
                 file.write("\n")
+    """
 else:
     raise Exception(f'input file in {in_path.absolute()} do not exist')
-
-
-def readMatrix(in_path):
-    """
-    read input matrix in csv format
-    example line1: 1,2,3
-    example line2: 4,5,6
-    example line3: 7,8,9
-    convert and output a 2-D list, matrix[[row],[row]...]
-    :param in_path :a file object which contain DTM table
-    :return: the list of input matrix
-    """
-    Matrix = list()
-    with open(in_path, mode='r', encoding='utf-8', newline='') as file:
-        reader = csv.reader(file, delimiter=',')  # Change delimiter if needed
-        #print(reader.fieldnames)
-        #x=1
-        for row in reader:
-            for i in range(len(row)):
-                row[i] = float(row[i])
-            Matrix.append(row)
-    return Matrix
-
-def GetCol(m:list, col: int):
-    return [x[col] for x in m]
-
-def mprint(m:list):
-    for x in m:
-        print(x)
-
-def vectMul(A:list,B:list):
-    sum = 0
-    for i in range(len(A)):
-        sum += A[i]*B[i]
-    return sum
-
-def Imatrix(size:int):
-    I = [[0 for _ in range(size)] for _ in range(size)]
-    for i in range(size):
-        I[i][i]=1
-    return I
-
-def madd(A:list,B:list):
-    sum = list()
-    if len(A) == len(B) and len(A[0]) == len(B[0]):
-        for i in range(len(A)):
-            sum.append([A[i][j]+B[i][j] for j in range(len(B[0]))])
-        return sum
-    else:
-        raise Exception(f' matrix dimension mismatch when doing multiplication')
-
-def msub(A:list,B:list):
-    sum = list()
-    if len(A) == len(B) and len(A[0]) == len(B[0]):
-        for i in range(len(A)):
-            sum.append([A[i][j]-B[i][j] for j in range(len(B[0]))])
-        return sum
-    else:
-        raise Exception(f' matrix dimension mismatch when doing multiplication')
-
-def scalarMul(A:list,k=float):
-    product = list()
-    for i in range(len(A)):
-        product.append([A[i][j]*k for j in range(len(A[0]))])
-    return product
-
-def mmul(A:list,B:list):
-    """
-    read 2 input matrix list
-    and return matrix multiplication product A*B
-    :param A: input matrix A
-    :param B: input matrix B
-    :return: a matrix product
-    """
-    product = list()
-    if len(A[0]) == len(B):
-        for i in range(len(A)):
-            product.append([vectMul(A[i],GetCol(B,j)) for j in range(len(B[0]))])
-        return product
-    else:
-        raise Exception(f' matrix dimension mismatch when doing multiplication')
-
-def partition(A:list, i:int, ii:int, j:int, jj:int):
-    """
-    read matrix list, 2 ranges, denoted by i:ii and j:jj
-    and return the partitioned matrix A with row i to row ii
-    and column j to jj 
-    :param A: input matrix A
-    :param i: starting row
-    :param ii: end row
-    :param j: starting col
-    :param jj: end col
-    :return: a matrix list
-    """
-    return([x[slice(j,jj)] for x in A[slice(i,ii)]])
