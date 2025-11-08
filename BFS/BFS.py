95510bc2ed6e995ff72e4bb202c7a67d5bee73e9
phoenix1ee <110279025+phoenix1ee@users.noreply.github.com>
2025-11-07 23:02:16 -0800

    finish BFS construct
    add graph read
    BFS construct with dict
    directed graph

--- BFS/BFS.py
+++ BFS/BFS.py
@@ -1,20 +1,53 @@
 import queue
+from pathlib import Path
+import csv
 
-class vertex:
-    def __init__(self,name):
-        self.d = None
-        self.label = name
-        self.pre = None
-        self.color = None
+def readgraph(in_path):
+    readin=dict()
+    with open(in_path, mode='r', encoding='utf-8', newline='') as file:
+        reader = csv.reader(file, delimiter=',')  # Change delimiter if needed
+        for row in reader:
+            if row:
+                if row[0] not in readin:
+                    readin.update({row[0]:[row[1]]})
+                else:
+                    readin[row[0]].append(row[1])
+    return readin
 
 Q = queue.Queue(-1)
 
-G = list()
-G.append({"a":["b","c","d"]})
-G.append({"b":["d"]})
-G.append({"c":["d"]})
+inpath = Path('testgraph.txt')
 
-root = vertex("a")
+"""
+adj = dict()
+adj.update({"a":["b","c"]})
+adj.update({"b":["a","d"]})
+adj.update({"c":["a","d"]})
+adj.update({"d":["b","c"]})
+"""
+adj = readgraph(inpath)
+G = dict()
+
+for keys in adj.keys():
+    G.update({keys:{"d":float('inf'),"pre":None}})
+
+visited = set()
+enqueued = set()
+root = "1"
+G[root]["d"]=0
 Q.put(root)
-while Q.not_empty():
-    pass
+enqueued.add(root)
+
+while not Q.empty():
+    current = Q.get()
+    if current not in visited:
+        visited.add(current)
+        print(current, G[current]["d"])
+        for x in adj[current]:
+            if x not in enqueued and x not in visited:
+                Q.put(x)
+                enqueued.add(x)
+                G[x]["d"]=G[current]["d"]+1
+                G[x]["pre"]=current
+
+print("finished")
---
+++ BFS/bipartitegraphGen.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import math
+import random
+import csv
+curdir = str(Path.cwd().absolute())
+
+size = [5]
+
+V = [x for x in range(1,size[0]+1)]
+print(f'V:{V}')
+output = []
+
+for i in range(size[0]):
+    Vcopy = V.copy()
+    adjacent = random.randint(1,5)
+    for j in range(adjacent):
+        output.append((i+1,Vcopy.pop(random.randint(0,len(Vcopy)-1))))
+
+out_path = Path(curdir + "\\testgraph" + ".txt")
+output_f = out_path.open('w')
+for k in output:
+    output_f.write(f'{k[0]},{k[1]}\n')
+output_f.close()
+
+readin=dict()
+in_path = Path(curdir + "\\testgraph" + ".txt")
+with open(in_path, mode='r', encoding='utf-8', newline='') as file:
+    reader = csv.reader(file, delimiter=',')  # Change delimiter if needed
+    for row in reader:
+        if row:
+            if row[0] not in readin:
+                readin.update({row[0]:[row[1]]})
+            else:
+                readin[row[0]].append(row[1])
+print(readin)
